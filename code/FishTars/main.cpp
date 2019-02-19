3f383afa958a237948f666e3f6d76fb3ca1852f7
leiyu <yuleics@outlook.com>
2019-02-19 09:23:16 +0800

    update

---
+++ code/FishTars/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <thread>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << ("hello!") << endl;
+
+    return 0;
+}
---
+++ code/FishTars/test/utiltest/test_exception.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "util/tc_ex.h"
+
+namespace lesliefishtest
+{
+    using namespace tars;
+    class ExceptionTest
+    {
+        explicit ExceptionTest() {};
+
+    public:
+        // 直接抛出异常
+        static void test()
+        {
+            throw TC_Exception("can't be called in the same thread");
+        }
+    };
+}
