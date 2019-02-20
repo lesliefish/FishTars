ecd5e9622b361ef150510610c9d772f49eed4910
leiyu <yuleics@outlook.com>
2019-02-20 10:49:59 +0800

    update

---
+++ code/FishTars/test/utiltest/test_thread.cpp
@@ -0,0 +1,75 @@
+﻿#include "test_thread.h"
+#include <thread>
+
+namespace lesliefishtest
+{
+    TestThread::TestThread()
+        : m_hasTerminate(false)
+    {
+
+    }
+
+    void TestThread::terminate()
+    {
+        m_hasTerminate = true;
+        {
+            TC_ThreadLock::Lock sync(*this);
+            notifyAll();
+        }
+    }
+
+    /****************************************!
+     * @brief  实际执行的任务
+     * @return void  
+     ****************************************/ 
+    void TestThread::runTask()
+    {
+        std::cout << "run the task" << endl;
+
+        // 休眠三秒后结束线程
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+        // 执调用终止结束线程执行
+        std::cout << "end the task" << endl;
+        terminate();
+    }
+
+    /****************************************!
+     * @brief  实现run纯虚函数
+     * @return void  
+     ****************************************/ 
+    void TestThread::run()
+    {
+        while (!m_hasTerminate)
+        {
+            runTask(); // 执行任务
+
+            {
+                TC_ThreadLock::Lock sync(*this);
+                timedWait(500);
+            }
+        }
+    }
+
+    /****************************************!
+     * @brief  静态测试函数
+     * @return void  
+     ****************************************/ 
+    void TestThread::test()
+    {
+        try
+        {
+            TestThread testThread;
+            testThread.start();		// 启动线程
+
+            cout << "thread id : " << testThread.getThreadControl().id() << endl; // 线程id
+
+            testThread.getThreadControl().join(); // 并入主线程
+        }
+        catch (const std::exception& ex)
+        {
+            cout << ex.what() << endl;
+        }
+    }
+
+}
--- code/FishTars/test/utiltest/test_thread.h
+++ code/FishTars/test/utiltest/test_thread.h
@@ -17,73 +17,22 @@ namespace lesliefishtest
     class TestThread : public TC_Thread, public TC_ThreadLock
     {
     public:
-        explicit TestThread()
-        {
-            m_hasTerminate = false;
-        }
-
-        void terminate()
-        {
-            m_hasTerminate = true;
-            {
-                TC_ThreadLock::Lock sync(*this);
-                notifyAll();
-            }
-        }
-
-        /************************************!
-         * @brief  实际执行的任务
-         * @return void
-         ************************************/
-        void runTask()
-        {
-            std::cout << "run the task" << endl;
-        }
+        explicit TestThread();
+        
+    private:
+        // 终止线程
+        void terminate();
+        // 执行实际任务
+        void runTask();
 
     protected:
-        /************************************!
-         * @brief  实现基类的纯虚函数
-         * @return void
-         ************************************/
-        virtual void run()
-        {
-            while (!m_hasTerminate)
-            {
-                runTask(); // 执行任务
+        virtual void run();
 
-                {
-                    TC_ThreadLock::Lock sync(*this);
-                    timedWait(1000);
-                }
-            }
-        }
-
-    protected:
+    private:
         bool m_hasTerminate; // 是否终止
 
-
-
+    // test part
     public:
-        /************************************!
-         * @brief  静态测试函数
-         * @return void
-         ************************************/
-        static void test()
-        {
-            try
-            {
-                TestThread testThread;
-                testThread.start();		// 启动线程
-
-                std::this_thread::sleep_for(std::chrono::seconds(5)); // 睡眠5秒
-
-                testThread.terminate(); // 终止
-                testThread.getThreadControl().join(); // 并入主线程
-            }
-            catch (const std::exception& ex)
-            {
-                cout << ex.what() << endl;
-            }
-        }
+        static void test();
     };
 }
