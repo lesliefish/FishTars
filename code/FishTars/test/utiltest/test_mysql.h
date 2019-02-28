4b6e3fc4c3289979ad896eb2ded205e17bae9b23
leiyu <yuleics@outlook.com>
2019-02-28 18:45:55 +0800

    code update

---
+++ code/FishTars/test/utiltest/test_mysql.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "../../tars/util/include/tc_mysql.h"
+#include <iostream>
+
+using namespace tars;
+using namespace std;
+
+namespace lesliefishtest
+{
+    class MySqlTest
+    {
+    public:
+        void testQuery()
+        {
+            TC_Mysql::MysqlData data{};
+            string strSql("select * from tarsMysql");
+            data = m_mysql.queryRecord(strSql);
+        }
+
+
+    private:
+        TC_Mysql m_mysql;
+
+    };
+}
